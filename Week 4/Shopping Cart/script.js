0ead1f9f53af025e05633b14376f50413de797ef
vaniseth <74897823+vaniseth@users.noreply.github.com>
2022-11-24 08:51:59 +0530

    week 4 labs

---
+++ Week 4/Shopping Cart/script.js
@@ -0,0 +1,35 @@
+$('.like-btn').on('click', function() {
+    $(this).toggleClass('is-active');
+ });
+
+ $('.minus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value &amp1) {
+        value = value - 1;
+    } 
+    else {
+        value = 0;
+    }
+ 
+  $input.val(value);
+ 
+});
+ 
+$('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value &amp100) {
+        value = value + 1;
+    } else {
+        value =100;
+    }
+ 
+    $input.val(value);
+});
